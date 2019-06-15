cea80e5b7104563f7558bb746b4c41cb02739d1a
Dan Barbarito <dan@barbarito.me>
2019-06-15 18:12:31 -0400

    mojo serve mithril app

--- lib/Startpage.pm
+++ lib/Startpage.pm
@@ -1,6 +1,12 @@
 package Startpage;
 use Mojo::Base 'Mojolicious';
 
+# This method is used to send back the index.html file
+sub reply_with_index {
+  my $c = shift;
+  $c->reply->static("index.html");
+}
+
 # This method will run once at server start
 sub startup {
   my $self = shift;
@@ -8,14 +14,20 @@ sub startup {
   # Load configuration from hash returned by config file
   my $config = $self->plugin('Config');
 
+  # Set static directory
+  my $client_folder = $self->app->home->child('client', 'dist');
+  push @{$self->app->static->paths}, $client_folder;
+
   # Configure the application
   $self->secrets($config->{secrets});
 
   # Router
   my $r = $self->routes;
 
-  # Normal route to controller
-  $r->get('/')->to('example#welcome');
+
+  # Re-route all index.html file
+  $r->get("/" => sub { reply_with_index(@_) });
+  $r->get("*" => sub { reply_with_index(@_) });
 }
 
 1;
