e3a0431c5193e652d314e48a1e461d2b379a88f4
Dan Barbarito <dan@barbarito.me>
2019-06-23 00:13:19 -0400

    backgrounds

--- client/src/js/components/App.jsx
+++ client/src/js/components/App.jsx
@@ -3,16 +3,29 @@ const m = require("mithril");
 const SearchEngine = require("./SearchEngine.jsx");
 const LocationInfoRow = require("./LocationInfoRow.jsx");
 
+// Generate the URL for the background
+const background = (function() {
+  const NUMBER_OF_BACKGROUNDS = 6;
+  const backgroundNumber =
+    Math.floor(Math.random() * NUMBER_OF_BACKGROUNDS) + 1;
+  return `/images/background${backgroundNumber}.jpg`;
+})();
+
 var App = {
   view: function() {
     return (
-      <main>
-        <LocationInfoRow />
-        <SearchEngine />
+      <main
+        style={`background:url('${
+          background
+        }');background-position:center;background-size:cover;`}
+      >
+        <div className="container">
+          <LocationInfoRow />
+          <SearchEngine />
+        </div>
       </main>
-    )
+    );
   }
-}
-
+};
 
 module.exports = App;
